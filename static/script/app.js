e8bbc1f3df8c4b1dfda1a27b906d47fad61c1964
nktech-official <backend.nktechical@gmail.com>
2021-04-20 01:12:31 +0530

    Arrow key to navigate through grid

--- static/script/app.js
+++ static/script/app.js
@@ -353,3 +353,37 @@ function act(id) {
     on();
     status = "started"
 }
+$(document).keydown(function(e) {
+    var $table = $(this);
+    var $active = $('input:focus,select:focus', $table);
+    var $next = null;
+    var focusableQuery = 'input:visible,select:visible,textarea:visible';
+    var position = parseInt($active.closest('td').index()) + 1;
+    console.log('position :', position);
+    switch (e.keyCode) {
+        case 37: // <Left>
+            $next = $active.parent('td').prev().find(focusableQuery);
+            break;
+        case 38: // <Up>                    
+            $next = $active
+                .closest('tr')
+                .prev()
+                .find('td:nth-child(' + position + ')')
+                .find(focusableQuery);
+
+            break;
+        case 39: // <Right>
+            $next = $active.closest('td').next().find(focusableQuery);
+            break;
+        case 40: // <Down>
+            $next = $active
+                .closest('tr')
+                .next()
+                .find('td:nth-child(' + position + ')')
+                .find(focusableQuery);
+            break;
+    }
+    if ($next && $next.length) {
+        $next.focus();
+    }
+});
